6f20ac94475a58e9c2a3b14bea197da9e1bdd33f
AndreasChristensen89 <brogaard89@gmail.com>
2021-09-09 08:29:00 +0000

    Set up db but with error

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -2,15 +2,15 @@ import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 if os.path.exists("env.py"):
-    import env # noqa - no quality assurance
+    import env  # noqa
 
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-db = SQLAlchemy(app)
+db = SQLAlchemy
 
 # The roots file that we're about to create will rely on the app files
 # as well as the db, therefore we don't import at the top
-from taskmanager import routes # noqa
+from taskmanager import routes  # noqa
