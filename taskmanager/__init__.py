482c6961cb7ce3180b452e7bd6bb1483676b24bf
AndreasChristensen89 <brogaard89@gmail.com>
2021-09-09 10:19:23 +0000

    Fix bug in init.py so I can review app

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -9,8 +9,6 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
-db = SQLAlchemy
+db = SQLAlchemy(app)
 
-# The roots file that we're about to create will rely on the app files
-# as well as the db, therefore we don't import at the top
 from taskmanager import routes  # noqa
--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,8 +1,5 @@
 from taskmanager import db
 
-# app = Flask(__name__)
-# db = SQLAlchemy(app)
-
 
 class Category(db.Model):
     # schema for the Category model
